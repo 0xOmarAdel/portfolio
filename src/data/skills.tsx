59e54dc2134a98fa17a7c97fe29e1ff9051eeb1f
Heisenberg <omaradelawad20013@gmail.com>
2023-10-27 16:12:01 +0200

    skills data file

---
+++ src/data/skills.tsx
@@ -0,0 +1,75 @@
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaReact,
+  FaPhp,
+  FaGitAlt,
+  FaGithub,
+  FaNpm,
+} from "react-icons/fa6";
+import { DiJavascript1 } from "react-icons/di";
+import {
+  SiTypescript,
+  SiTailwindcss,
+  SiRedux,
+  SiJest,
+  SiEslint,
+  SiDaisyui,
+  SiMysql,
+} from "react-icons/si";
+import { BsBootstrap } from "react-icons/bs";
+import { PiFramerLogo } from "react-icons/pi";
+
+export const skills = [
+  {
+    icon: <FaHtml5 />,
+  },
+  {
+    icon: <FaCss3Alt />,
+  },
+  {
+    icon: <DiJavascript1 className="scale-110" />,
+  },
+  {
+    icon: <SiTypescript className="scale-90" />,
+  },
+  {
+    icon: <SiTailwindcss />,
+  },
+  {
+    icon: <FaReact />,
+  },
+  {
+    icon: <SiRedux />,
+  },
+  {
+    icon: <BsBootstrap />,
+  },
+  {
+    icon: <SiJest />,
+  },
+  {
+    icon: <SiEslint />,
+  },
+  {
+    icon: <SiDaisyui className="scale-[1.5]" />,
+  },
+  {
+    icon: <FaPhp className="scale-125" />,
+  },
+  {
+    icon: <SiMysql className="scale-[1.4]" />,
+  },
+  {
+    icon: <PiFramerLogo />,
+  },
+  {
+    icon: <FaGitAlt />,
+  },
+  {
+    icon: <FaGithub />,
+  },
+  {
+    icon: <FaNpm className="scale-125" />,
+  },
+];
