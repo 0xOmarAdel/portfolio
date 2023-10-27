97b56bf9cd278617e2132277a7a0a05aa0b33bfc
Heisenberg <omaradelawad20013@gmail.com>
2023-10-27 16:15:49 +0200

    socials data file

---
+++ src/data/socials.tsx
@@ -0,0 +1,12 @@
+import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
+
+export const socials = [
+  {
+    icon: <FaGithub />,
+    path: "",
+  },
+  {
+    icon: <FaLinkedinIn />,
+    path: "",
+  },
+];
