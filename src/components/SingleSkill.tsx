5ce94e8aace20ed1ef6a0152b229ab3e4cf4fd3d
Heisenberg <omaradelawad20013@gmail.com>
2023-10-28 04:25:17 +0200

    created SingleSkill.tsx file

---
+++ src/components/SingleSkill.tsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+interface Props {
+  icon: React.ReactNode;
+  index: number;
+}
+
+const SingleSkill = ({ icon: Icon, index }: Props) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  const imageVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  const animationDelay = 0.2;
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      variants={imageVariants}
+      animate={inView ? "visible" : "hidden"}
+      custom={index}
+      transition={{ delay: index * animationDelay }}
+      className="h-full px-24 py-9 border border-violet-600 rounded-md text-5xl text-white"
+    >
+      {Icon}
+    </motion.div>
+  );
+};
+
+export default SingleSkill;
