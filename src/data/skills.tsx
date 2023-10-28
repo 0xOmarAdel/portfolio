9f3bc98b7f2d437700bdd0ea0e83c210aaa495ee
Heisenberg <omaradelawad20013@gmail.com>
2023-10-28 04:23:31 +0200

    added id to each obj of the skills arr of objs

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -22,54 +22,71 @@ import { PiFramerLogo } from "react-icons/pi";
 
 export const skills = [
   {
+    id: 1,
     icon: <FaHtml5 />,
   },
   {
+    id: 2,
     icon: <FaCss3Alt />,
   },
   {
+    id: 3,
     icon: <DiJavascript1 className="scale-110" />,
   },
   {
+    id: 4,
     icon: <SiTypescript className="scale-90" />,
   },
   {
+    id: 5,
     icon: <SiTailwindcss />,
   },
   {
+    id: 6,
     icon: <FaReact />,
   },
   {
+    id: 7,
     icon: <SiRedux />,
   },
   {
+    id: 8,
     icon: <BsBootstrap />,
   },
   {
+    id: 9,
     icon: <SiJest />,
   },
   {
+    id: 10,
     icon: <SiEslint />,
   },
   {
+    id: 11,
     icon: <SiDaisyui className="scale-[1.5]" />,
   },
   {
+    id: 12,
     icon: <FaPhp className="scale-125" />,
   },
   {
+    id: 13,
     icon: <SiMysql className="scale-[1.4]" />,
   },
   {
+    id: 14,
     icon: <PiFramerLogo />,
   },
   {
+    id: 15,
     icon: <FaGitAlt />,
   },
   {
+    id: 16,
     icon: <FaGithub />,
   },
   {
+    id: 17,
     icon: <FaNpm className="scale-125" />,
   },
 ];
