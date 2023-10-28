13b40f94b3efe38789c1a4fe25e051fc9df25ac5
Heisenberg <omaradelawad20013@gmail.com>
2023-10-28 04:35:02 +0200

    created SkillsText.tsx file

---
+++ src/components/SkillsText.tsx
@@ -0,0 +1,17 @@
+const SkillsText = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-6">
+      <div className="text-5xl text-white font-bold text-center">
+        Professional{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+          Skillset
+        </span>
+      </div>
+      <div className="cursive text-2xl text-gray-200 text-center">
+        Making apps with modern technologies
+      </div>
+    </div>
+  );
+};
+
+export default SkillsText;
