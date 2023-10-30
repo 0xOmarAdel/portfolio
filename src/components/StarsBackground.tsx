dc36b9e57db5a9e6804666d775965d389d30ea8c
Heisenberg <omaradelawad20013@gmail.com>
2023-10-30 14:37:44 +0200

    deleted props in the StarsBackground component

--- src/components/StarsBackground.tsx
+++ src/components/StarsBackground.tsx
@@ -4,20 +4,20 @@ import { Points, PointMaterial } from "@react-three/drei";
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
-const StarBackground = (props: any) => {
+const StarBackground = () => {
   const ref: any = useRef();
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
 
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3}>
         <PointMaterial
           transparent
           color="$fff"
