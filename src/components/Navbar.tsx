668142ce8edd96a62ea0e46bb20a33fead029be0
Heisenberg <omaradelawad20013@gmail.com>
2023-10-31 05:08:31 +0200

    Navbar tabs framer motion animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import { socials } from "../data/socials";
 import { navLinks } from "../data/navLinks";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState(navLinks[0]);
+
   return (
     <div className="fixed top-0 z-50 w-full px-20 py-4 bg-[#03001417] shadow-lg shadow-[#2A0E61]/50 backdrop-blur-md">
       <div className="w-full h-full m-auto px-[10px] flex flex-row items-center justify-between">
@@ -11,15 +15,27 @@ const Navbar = () => {
           </span>
         </a>
 
-        <div className="md:mr-24 flex flex-row items-center justify-between">
-          <div className="mr-[15px] px-12 py-2 flex items-center justify-between gap-10 bg-[#0300145e] border border-[#7042f861] rounded-full text-gray-200">
+        <nav className="md:mr-24 flex flex-row items-center justify-between">
+          <ul className="mr-[15px] flex items-center justify-between bg-[#0300145e] border border-violet-900 rounded-full text-gray-200 overflow-hidden">
             {navLinks.map((navLink) => (
-              <a key={navLink.id} href={`#${navLink.id}`}>
-                {navLink.text}
-              </a>
+              <li
+                key={navLink.id}
+                className="relative py-2 px-6 first-of-type:pl-8 last-of-type:pr-8"
+                onClick={() => setActiveLink(navLink)}
+              >
+                <a href={`#${navLink.id}`} className="relative z-10">
+                  {navLink.text}
+                </a>
+                {navLink === activeLink ? (
+                  <motion.div
+                    className="absolute inset-0 w-full h-full bg-violet-900"
+                    layoutId="nav-links"
+                  />
+                ) : null}
+              </li>
             ))}
-          </div>
-        </div>
+          </ul>
+        </nav>
 
         <div className="flex flex-row gap-5 text-xl text-white">
           {socials.map((social) => (
