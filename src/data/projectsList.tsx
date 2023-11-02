ffcc1dc8983c92e599d77c667b0cf16d57a170f1
Heisenberg <omaradelawad20013@gmail.com>
2023-11-02 11:52:51 +0200

    projects list data file

---
+++ src/data/projectsList.tsx
@@ -0,0 +1,157 @@
+import socioApp from "../assets/shopNest.png";
+import shopNest from "../assets/shopNest.png";
+import movieMaze from "../assets/movieMaze.png";
+import cms from "../assets/movieMaze.png";
+import dreamRestaurant from "../assets/movieMaze.png";
+import bookHaven from "../assets/movieMaze.png";
+
+export const projectsList = [
+  {
+    id: 1,
+    title: "Socio App",
+    description:
+      "Socio is a powerful and feature-rich social media application that allows users to connect, share, and engage with their friends and followers. With Socio, you can seamlessly enjoy all the essential social media features, from following and unfollowing users to posting updates, commenting on posts, uploading photos, managing your profile, and much more.",
+    image: socioApp,
+    link: "https://sociooapp.netlify.app",
+    github: "https://github.com/MrGhost23/socio-app",
+    collaborators: [
+      {
+        username: "MrGhost23",
+        image: "https://avatars.githubusercontent.com/u/72778139",
+      },
+    ],
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "React",
+      "React Router",
+      "Redux Toolkit",
+      "Tailwind",
+      "Axios",
+      "Node JS",
+      "Express",
+      "Mongo DB",
+      "Socket IO",
+    ],
+  },
+  {
+    id: 2,
+    title: "Shop Nest",
+    description:
+      "Shop Nest is a feature-rich ecommerce web app offering a wide range of products. Browse categories, apply filters, search, and enjoy a secure checkout. Wishlist items, manage profile, addresses, orders, and reviews. Experience seamless shopping with authentication and personalized recommendations.",
+    image: shopNest,
+    link: "https://shop-nest-app.netlify.app",
+    github: "https://github.com/0xOmarAdel/shop-nest",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "React Router",
+      "Redux Toolkit",
+      "Tailwind",
+      "Firebase",
+      "Firestore",
+    ],
+  },
+  {
+    id: 3,
+    title: "Movie Maze",
+    description:
+      "Movie Maze App is a React.js web application that utilizes the TMDB API to provide users with a seamless experience of exploring movies and TV series. Discover, search, and view details of your favorite titles with ease. Find top-rated movies, popular shows, and the best providers.",
+    image: movieMaze,
+    link: "https://movie-maze-app.netlify.app",
+    github: "https://github.com/0xOmarAdel/movie-maze",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "React",
+      "React Router",
+      "Tailwind",
+      "Axios",
+    ],
+  },
+  {
+    id: 4,
+    title: "Complaint Management System",
+    description:
+      "The Complaint Management System is a web-based application designed to help organizations efficiently handle and track customer complaints and feedback. This system streamlines the process of receiving, documenting, and resolving complaints, ensuring a better customer experience and improved service quality.",
+    image: cms,
+    link: "https://cms-complaints-app.netlify.app",
+    github: "https://github.com/0xOmarAdel/cms",
+    collaborators: [
+      {
+        username: "MrGhost23",
+        image: "https://avatars.githubusercontent.com/u/72778139",
+      },
+    ],
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "React Router",
+      "Redux Toolkit",
+      "Tailwind",
+      "Axios",
+      "Node JS",
+      "Express",
+      "Mongo DB",
+    ],
+  },
+  {
+    id: 5,
+    title: "Dream Restaurant",
+    description: "Dream Restaurant.",
+    image: dreamRestaurant,
+    link: "https://dream-restaurant.netlify.app",
+    github: "https://github.com/0xOmarAdel/dream",
+    collaborators: [
+      {
+        username: "MrGhost23",
+        image: "https://avatars.githubusercontent.com/u/72778139",
+      },
+      {
+        username: "noahwillson",
+        image: "https://avatars.githubusercontent.com/u/79482929",
+      },
+    ],
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "React Router",
+      "Redux Toolkit",
+      "Tailwind",
+      "Daisy UI",
+      "Axios",
+      "Node JS",
+      "Express",
+      "Mongo DB",
+    ],
+  },
+  {
+    id: 6,
+    title: "Book Haven",
+    description: "Book Haven.",
+    image: bookHaven,
+    link: "https://book-haven-app.netlify.app",
+    github: "https://github.com/0xOmarAdel/book-haven",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "React",
+      "React Router",
+      "React Lottie",
+      "Tailwind",
+      "Framer Motion",
+    ],
+  },
+];
