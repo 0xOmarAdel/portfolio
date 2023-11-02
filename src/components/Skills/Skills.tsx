183810edf23fc43af4e3bf514f66d49cdef09cc7
Heisenberg <omaradelawad20013@gmail.com>
2023-11-02 13:51:11 +0200

    updated Skills.tsx

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,7 +1,7 @@
 import SingleSkill from "./SingleSkill";
 import { skills } from "../../data/skills";
 import Section from "../../ui/Section";
-import SectionHeading from '../../ui/SectionHeading';
+import SectionHeading from "../../ui/SectionHeading";
 
 const Skills = () => {
   return (
@@ -10,11 +10,11 @@ const Skills = () => {
       className="flex flex-col items-center justify-center gap-10"
     >
       <div className="flex flex-col items-center justify-center gap-6">
-      <SectionHeading text="Professional" gradient="Skillset" />
-      <div className="cursive text-2xl text-gray-200 text-center">
-        Making apps with modern technologies
+        <SectionHeading text="Professional" gradient="Skillset" />
+        <div className="cursive text-2xl text-gray-200 text-center">
+          Making apps with modern technologies
+        </div>
       </div>
-    </div>
       <div className="flex flex-row flex-wrap items-center justify-center gap-10">
         {skills.map((image, index) => (
           <SingleSkill key={image.id} icon={image.icon} index={index} />
