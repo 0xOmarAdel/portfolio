3acd7aa89bba54fb83a7329be80760806fc59900
Heisenberg <omaradelawad20013@gmail.com>
2023-11-02 13:47:44 +0200

    updated projects grid on small screens

--- src/components/Projects/ProjectsGrid.tsx
+++ src/components/Projects/ProjectsGrid.tsx
@@ -3,7 +3,7 @@ import { projectsList } from "../../data/projectsList";
 
 const ProjectsGrid = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-14">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-14">
       {projectsList.map((project) => (
         <ProjectsItem key={project.id} {...project} />
       ))}
