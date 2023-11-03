45ec40f3a8f0e2914b1cc78eab43267922a5a842
Heisenberg <omaradelawad20013@gmail.com>
2023-11-03 09:13:16 +0200

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import About from "./components/About/About.tsx";
+import Contact from "./components/Contact/Contact.tsx";
+import Footer from "./components/Footer.tsx";
+import Hero from "./components/Hero/Hero.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Projects from "./components/Projects/Projects.tsx";
+import Skills from "./components/Skills/Skills.tsx";
+import StarsCanvas from "./components/StarsBackground.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <StarsCanvas />
+      <div className="relative z-50 flex flex-col">
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
