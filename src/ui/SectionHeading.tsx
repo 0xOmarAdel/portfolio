d8cbc8be6bf1ea9a186d35987399e800348ae7fd
Heisenberg <omaradelawad20013@gmail.com>
2023-11-05 11:12:53 +0200

    section heading text size on small screens

--- src/ui/SectionHeading.tsx
+++ src/ui/SectionHeading.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const SectionHeading: React.FC<Props> = ({ text, gradient }) => {
   return (
-    <div className="text-5xl text-white font-bold text-center">
+    <div className="text-4xl md:text-5xl text-white font-bold text-center">
       {text}{" "}
       <span className="text-transparent bg-clip-text bg-gradient-to-r from-violet-400 to-violet-700">
         {gradient}
