7f44212b38ee829476ffd9f5991b41a43e647c23
Heisenberg <omaradelawad20013@gmail.com>
2023-11-05 11:15:56 +0200

    added padding top to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="pb-5 flex flex-row justify-center text-lg capitalize">
+    <div className="pt-10 pb-5 flex flex-row justify-center text-lg capitalize">
       Copyright © {currentYear} {name}
     </div>
   );
