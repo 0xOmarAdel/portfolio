6dbf034b2190e4c2dcb836e8dec1ab21bba7b361
Heisenberg <omaradelawad20013@gmail.com>
2023-11-06 16:40:32 +0200

    contact section lottie animation

--- src/components/Contact/ContactImage.tsx
+++ src/components/Contact/ContactImage.tsx
@@ -1,23 +1,40 @@
 import { motion } from "framer-motion";
-import mail from "../../assets/mail.svg";
 import { slideInFromBottomVariants } from "../../utils/motion";
 import { useInView } from "react-intersection-observer";
+import contactAnimation from "../../assets/contactAnimation.json";
+import Lottie from "lottie-react";
+import { useEffect, useRef } from "react";
+import { LottieRefCurrentProps } from "lottie-react";
 
 const ContactImage: React.FC = () => {
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
 
+  const animationRef = useRef<LottieRefCurrentProps>(null);
+
+  useEffect(() => {
+    if (inView) {
+      animationRef.current!.play();
+    }
+  }, [inView]);
+
   return (
-    <motion.img
+    <motion.div
       initial="hidden"
       animate={inView ? "visible" : "hidden"}
       variants={slideInFromBottomVariants(50, 0.5, 0.5)}
-      src={mail}
-      alt=""
-      className="order-1 lg:order-2"
       ref={ref}
-    />
+      className="order-1 xl:order-2 shrink-0 w-full mx-auto xl:mx-0"
+    >
+      <Lottie
+        lottieRef={animationRef}
+        animationData={contactAnimation}
+        autoplay={false}
+        loop={true}
+        style={{ width: "100%" }}
+      />
+    </motion.div>
   );
 };
 
