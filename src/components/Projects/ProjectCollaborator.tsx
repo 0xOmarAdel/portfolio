bc852106a1cb710fa16670c66c08c178000b18ee
Heisenberg <omaradelawad20013@gmail.com>
2023-11-09 16:51:26 +0200

    fetching projects collaborators images

--- src/components/Projects/ProjectCollaborator.tsx
+++ src/components/Projects/ProjectCollaborator.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+
 type Props = {
   collaborator: { username: string; image: string };
 };
 
 const ProjectCollaborator: React.FC<Props> = ({ collaborator }) => {
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    let objectURL = "";
+    const loadImage = async () => {
+      try {
+        const response = await fetch(collaborator.image);
+        const blob = await response.blob();
+        objectURL = URL.createObjectURL(blob);
+        setImageSrc(objectURL);
+      } catch (error) {
+        console.error("Failed to fetch:", collaborator.image);
+      }
+    };
+
+    loadImage();
+
+    return () => {
+      URL.revokeObjectURL(objectURL);
+    };
+  }, [collaborator.image]);
+
   return (
     <a
       key={collaborator.username}
@@ -10,11 +34,13 @@ const ProjectCollaborator: React.FC<Props> = ({ collaborator }) => {
       target="_blank"
       aria-label={`${collaborator.username} github account`}
     >
-      <img
-        src={collaborator.image}
-        alt={`${collaborator.username}'s avatar`}
-        className="w-5 h-5 rounded-full"
-      />
+      {imageSrc && (
+        <img
+          src={imageSrc}
+          alt={`${collaborator.username}'s avatar`}
+          className="w-5 h-5 rounded-full"
+        />
+      )}
     </a>
   );
 };
