10441e112b6530681942ede0a501313c22b04667
Omar Adel <omaradelawad20013@gmail.com>
2024-04-25 02:37:51 +0200

    updated the education about section style

--- src/components/About/AboutEducation.tsx
+++ src/components/About/AboutEducation.tsx
@@ -3,15 +3,15 @@ import { education } from "../../data/text";
 
 const AboutEducation = () => {
   return (
-    <div className="flex flex-row justify-center xl:justify-start gap-4">
+    <div className="flex flex-row justify-center gap-4 xl:justify-start">
       <div className="relative">
-        <div className="h-fit p-2 rounded-md bg-gradient-to-r from-violet-400 to-violet-700 translate-y-2">
-          <FaGraduationCap className="text-lg md:text-xl" />
+        <div className="flex items-center justify-center p-2 translate-y-2 rounded-md w-9 h-9 bg-gradient-to-r from-violet-400 to-violet-700">
+          <FaGraduationCap className="text-xl" />
         </div>
         <div className="absolute top-2 left-1/2 -z-10 h-[calc(100%-0.5rem)] w-[3px] bg-gradient-to-r from-violet-400 to-violet-700 rounded-bl-full rounded-br-full -translate-x-1/2"></div>
       </div>
       <div className="flex flex-col gap-0.5">
-        <h3 className="text-lg md:text-xl font-semibold">{education.degree}</h3>
+        <h3 className="text-lg font-semibold md:text-xl">{education.degree}</h3>
         <p className="mt-1">{education.college}</p>
         <p className="mt-0.5 text-sm text-gray-400">
           {education.started} – {education.graduated}
