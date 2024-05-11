f40ded2bf78fec4d85667dcda78e573de057a452
Omar Adel <omaradelawad20013@gmail.com>
2024-05-11 17:37:42 +0300

    updated the skill box size

--- src/components/Skills/SingleSkill.tsx
+++ src/components/Skills/SingleSkill.tsx
@@ -34,10 +34,10 @@ const SingleSkill = ({ inView, icon: Icon, text, index }: Props) => {
       animate={inView ? "visible" : "hidden"}
       custom={index}
       transition={{ delay: index * animationDelay }}
-      className="flip-card basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 relative h-full w-full"
+      className="flip-card relative text-center"
     >
       <motion.div
-        className="flip-card-inner w-22 h-28 sm:h-36 mx-3 sm:mx-5 mb-6 sm:mb-10 cursor-pointer"
+        className="flip-card-inner w-28 sm:w-36 h-28 sm:h-36 mx-3 sm:mx-5 mb-6 sm:mb-10 cursor-pointer"
         initial={false}
         animate={{ rotateY: isFlipped ? 180 : 360 }}
         transition={{ duration: 0.6, animationDirection: "normal" }}
@@ -47,7 +47,7 @@ const SingleSkill = ({ inView, icon: Icon, text, index }: Props) => {
         <div className="flip-card-front w-full h-full flex flex-row items-center justify-center border-[2px] border-violet-700 border-opacity-60 rounded-md text-4xl sm:text-5xl text-white">
           {Icon}
         </div>
-        <div className="flip-card-back w-full h-full flex flex-row items-center justify-center bg-violet-700 rounded-md text-xl sm:text-2xl text-white">
+        <div className="flip-card-back w-full h-full flex flex-row items-center justify-center bg-violet-700 rounded-md text-base sm:text-xl text-white">
           {text}
         </div>
       </motion.div>
